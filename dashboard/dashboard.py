3ce24eff290cbe7df669f372ed141445d5bb2fe1
Arif Kurniawan <ak2820835@gmail.com>
2025-03-15 03:44:28 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,16 +5,6 @@ import seaborn as sns
 import streamlit as st
 import os
 
-def load_data(file_path):
-    if os.path.exists(file_path):
-        st.write(f" File ditemukan: {file_path}")
-        return pd.read_csv(file_path)
-    else:
-        st.error(f" File tidak ditemukan: {file_path}. Pastikan file berada di folder 'data/' dan path sudah benar.")
-        return None
-
-day_df = load_data("data/day.csv")
-hour_df = load_data("data/hour.csv")
 
 if day_df is not None and hour_df is not None:
     st.write("### Data Day")

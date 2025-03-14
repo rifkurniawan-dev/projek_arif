9e90e563b76313537e2a0b3550125f1bb56d929e
Arif Kurniawan <ak2820835@gmail.com>
2025-03-14 22:48:47 +0700

    Update proyek_analisis_data_arif (2).py

--- proyek_analisis_data_arif (2).py
+++ proyek_analisis_data_arif (2).py
@@ -23,6 +23,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st 
+import plotly.express as px
+import plotly.graph_objects as go
 
 """## Data Wrangling
 

30f517a3053c65f3f19e09327733754f4dcb8247
Arif Kurniawan <ak2820835@gmail.com>
2025-03-15 03:01:25 +0700

    Update proyek_analisis_data_arif (2).py

--- proyek_analisis_data_arif (2).py
+++ proyek_analisis_data_arif (2).py
@@ -23,16 +23,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
 
 """## Data Wrangling
 
 ### Gathering Data
 """
 
-day_df = pd.read_csv("day.csv")
+day_df = pd.read_csv("data/day.csv")
 day_df.head()
 
-hour_df = pd.read_csv("hour.csv")
+hour_df = pd.read_csv("data/hour.csv")
 hour_df.head()
 
 """### Assessing Data

e4195eca2922719e8e125a29af7ced9ecd91dbad
Arif Kurniawan <ak2820835@gmail.com>
2025-03-21 23:06:09 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -72,14 +72,20 @@ if data is not None:
         st.pyplot(plt)
 
         # Grafik Pengaruh Musim terhadap Penyewaan
-        seasonal_influence = filtered_data.groupby('Musim')['cnt_x'].sum().reset_index().sort_values(by='cnt_x', ascending=False)
-        plt.figure(figsize=(10, 6))
-        sns.barplot(x='Musim', y='cnt_x', data=seasonal_influence, palette='cool')
-        plt.title('Pengaruh Musim Terhadap Jumlah Penyewaan Sepeda', fontsize=16)
-        plt.xlabel('Musim', fontsize=14)
-        plt.ylabel('Total Penyewaan Sepeda', fontsize=14)
-        st.pyplot(plt)
-
+       weather_influence = hour_day_df.groupby('weathersit_x')['cnt_x'].sum().sort_values(ascending=False).reset_index()
+weather_mapping = {
+    1: 'Cerah/Sedikit Berawan',
+    2: 'Berkabut/Berawan',
+    3: 'Hujan Ringan/Snow Ringan',
+    4: 'Hujan Deras/Snow Lebat'
+}
+hour_df['weathersit'] = hour_df['weathersit'].map(weather_mapping)
+plt.figure(figsize=(12, 6))
+sns.boxplot(x='weathersit', y='cnt', data=hour_df)
+plt.title('pengaruh Penyewaan Sepeda Berdasarkan Kondisi Cuaca')
+plt.xlabel('Kondisi Cuaca (weathersit)')
+plt.ylabel('Jumlah Penyewaan Sepeda')
+st.pyplot(plt)
     except Exception as e:
         st.error(f'Terjadi kesalahan dalam pemrosesan data: {e}')
 

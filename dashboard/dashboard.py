62a960ef33c940dd5523bd0f8c4064b87d80521a
Arif Kurniawan <ak2820835@gmail.com>
2025-03-22 13:09:34 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -95,10 +95,12 @@ if not seasonal_influence.empty:
     seasonal_influence['Musim'] = seasonal_influence['season_x'].map(musim_mapping)
 
     fig, ax = plt.subplots(figsize=(10, 6))
-    sns.barplot(x='Musim', y='cnt_x', data=seasonal_influence, palette="Blues", ax=ax)
-    ax.set_title('Pengaruh Musim Terhadap Jumlah Penyewaan Sepeda', fontsize=16)
-    ax.set_xlabel('Musim', fontsize=14)
-    ax.set_ylabel('Total Penyewaan Sepeda', fontsize=14)
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='Musim', y='cnt_x', data=seasonal_influence, hue='Musim', dodge=False, palette="Blues")
+    plt.title('Pengaruh Musim Terhadap Jumlah Penyewaan Sepeda', fontsize=16)
+    plt.xlabel('Musim', fontsize=14)
+    plt.ylabel('Total Penyewaan Sepeda', fontsize=14)
+    plt.legend([], [], frameon=False)
     st.pyplot(fig)
 else:
     st.warning("Tidak ada data yang ditemukan untuk ditampilkan.")
